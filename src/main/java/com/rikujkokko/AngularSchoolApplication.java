2720954c41213e26bba112ebc8ba777212c6ba5b
rjkokko <riku.kokko@gofore.com>
2015-11-03 22:50:18 +0200

    Second commit

---
+++ src/main/java/com/rikujkokko/AngularSchoolApplication.java
@@ -0,0 +1,12 @@
+package com.rikujkokko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularSchoolApplication.class, args);
+    }
+}
